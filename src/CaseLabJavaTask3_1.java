debec1f0ae0131c4b32593a84f5c343e17fec147
Andrei Shumov <staydoomed@gmail.com>
2023-09-01 16:12:46 +0300

    Initial commit

---
+++ src/CaseLabJavaTask3_1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public static void main(String[] args) {
+        Node node=new Node(1,new Node(2,new Node(3,new Node(4))));
+        Node.printLinkedList(node);
+        Node reversedNode= Node.reverseList(node);
+        Node.printLinkedList(reversedNode);
+
+        Node node2=null;
+        Node.printLinkedList(node2);
+        Node reversedNode2 =Node.reverseList(node2);
+        Node.printLinkedList(reversedNode2);
+    }
+}
